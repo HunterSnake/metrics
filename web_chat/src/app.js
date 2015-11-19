148b1a900ff594b7ae1356b1183353fbff46b791
HunterSnake <690794889@qq.com>
2015-11-19 11:21:13 +0800

    chart for use new data service

--- web_chat/src/app.js
+++ web_chat/src/app.js
@@ -23,7 +23,7 @@ angular.module('myApp', ['myChart'])
 
       // Source of the log file
       //src: 'http://localhost:3000/files/access.log',
-      src: 'http://localhost:8008/collections/MetricAggregatesDailyView/AggregateId',
+      src: 'http://localhost:8009/collections/MetricAggregatesDailyView/AggregateId',
       //src: 'http://metrics-datasync-service.15.126.130.219.xip.io/collections/MetricAggregatesDailyView/AggregateId',
 
       // Data entries
